5a7d7412bfd2b6b49c567b93bcb92740787b50dc
jaideep-c <jaideepcvl@gmail.com>
2023-01-18 23:22:39 +0530

    created index page

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+import React from "react";
+
+
+const Home:NextPage=()=>{
+    return (
+        <h1>
+            Hello team
+        </h1>
+    )
+}
+
+export default Home;
