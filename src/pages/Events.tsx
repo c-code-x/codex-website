7ba72f35168da282c82e5116264378a6a8b68107
Suhiteswar <105535366+suhit123@users.noreply.github.com>
2023-02-26 14:01:02 +0530

    made some changes in event page

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,12 +1,9 @@
 
 import memory from "../assests/memories/mem_event1.png";
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
 import Image from "next/image";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
 import styles from '../styles/event.module.css'
-import Glimpse from "@/components/Glimpse";
 import EventDate from "@/components/Calender";
 import Event_curosel from "@/components/Eventspo_carousel";
 const Eventspage =()=>{
