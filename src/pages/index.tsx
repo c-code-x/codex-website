a158f4b77c169d74ee4e6da4f16e7e0e8341a096
breathecode6365 <pdandlur@gitam.in>
2023-03-12 19:39:41 +0530

    Updated SEO META INFO

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,11 +18,13 @@ const Home:NextPage=()=>{
                 <meta name="title" content="Codex - Club"/>
                     <meta name="description" content="CODEX is a student-run organization at the University of GITAM that aims to provide students with the resources and opportunities to learn about computer science and software development. We learn, we build, and we share!"/>
                         <meta name="keywords" content="codex, gitam, codex website, technical club, programming, coding club, gitam club, club, student club, active student gitam student clubs,  technology"/>
-                                <meta name="language" content="English"/>
-                                    <meta name="revisit-after" content="3 days"/>
-                                        <meta name="author" content="CodeX - CoreTeam"/>
-                                            <meta name="distribution" content="global"/>
-                                                <meta name="rating" content="general"/>
+                         <meta name="language" content="English"/>
+                         <meta name="revisit-after" content="3 days"/>
+                         <meta name="author" content="CodeX - CoreTeam"/>
+                         <meta name="distribution" content="global"/>
+                         <meta name="rating" content="general"/>
+                <meta property="og:title" content="Codex - Cub"/>
+                <meta property="og:description" content="CODEX is a student-run organization at the University of GITAM that aims to provide students with the resources and opportunities to learn about computer science and software development. We learn, we build, and we share!"/>
 
             </Head>
             <NavBar/>
