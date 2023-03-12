269c9f5cdc1e0af7b538f6912c49ec921be6402c
jaideep-c <jaideepcvl@gmail.com>
2023-03-12 21:19:30 +0530

    prettier rules

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+// prettier.config.js
+module.exports = {
+    printWidth: 100,
+    tabWidth: 4,
+    semi: true,
+    singleQuote: false,
+    bracketSpacing: true,
+    bracketSameLine: true,
+    arrowParens: "always",
+};
