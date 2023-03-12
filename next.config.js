83f3b827fb41c73d3cc24a8325872f02df67c45a
jaideep-c <jaideepcvl@gmail.com>
2023-03-12 21:41:27 +0530

    fix: images by url were not allowed by next

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ["user-images.githubusercontent.com"],
+    },
+};
 
 module.exports = nextConfig;
