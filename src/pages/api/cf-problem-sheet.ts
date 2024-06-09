6c8b0cbb22adae8fad14c3d0dc7284776e122bb7
breathecode6365 <pdandlur@gitam.in>
2024-06-10 00:17:59 +0530

    CORS fix

--- src/pages/api/cf-problem-sheet.ts
+++ src/pages/api/cf-problem-sheet.ts
@@ -1,9 +1,17 @@
 import CodeforcesAPI from "@/core/codeforces-api/CodeforcesAPI";
 import { NextApiRequest, NextApiResponse } from "next";
+import corsMiddleware from "@/cors-middleware";
 
 const getCodeforcesProblemSheet = async (req: NextApiRequest, res: NextApiResponse) => {
+    await new Promise<void>((resolve, reject) => {
+        corsMiddleware(req, res, (err: any) => {
+            if (err) {
+                return reject(err);
+            }
+            resolve();
+        });
+    });
     const { handle, minProblemRating, maxProblemRating, numberOfPreviousContests } = req.query;
-    // Check if handle is present
     if (!handle) {
         return res.status(400).json({ error: "Bad Request: handle is required" });
     }
